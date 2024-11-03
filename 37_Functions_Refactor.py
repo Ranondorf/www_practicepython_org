ef29e3c4a41bd2036fe5c674f41d6dcaf988898d
Rui <rui7@tuta.io>
2024-11-03 22:58:32 +1100

    Completed all 1 and 2 chili exercises.

--- 37_Functions_Refactor.py
+++ 37_Functions_Refactor.py
@@ -1,3 +1,26 @@
 #!/usr/bin/python
 
 
+def draw_vertline(number):
+    """Draw vertical line"""
+    for i in range(number):
+        print(" ---", end="")
+    print()
+
+
+def draw_horiline(number):
+    """Draw horizontal line"""
+    for i in range(number):
+        print("|   ", end="")
+    print("|")
+
+
+def draw_grid(number):
+    """Draw a grid"""
+    for i in range(number):
+        draw_vertline(number)
+        draw_horiline(number)
+    draw_vertline(number)
+
+
+draw_grid(5)
