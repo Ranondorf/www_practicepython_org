a9a674f8fa0d0ef29fa2a4d2027459f6d96741b7
Rui <rui7@tuta.io>
2024-11-06 16:10:42 +1100

    Completed web decode 1

--- 17_Decode_A_Web_Page.py
+++ 17_Decode_A_Web_Page.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python
 
 
+from bs4 import BeautifulSoup
+import requests
+
+
+def parser(url):
+    req = requests.get(url).text
+    soup = BeautifulSoup(req, 'lxml')
+    for article in soup.find_all('article'):
+        print(article.h4.a.text)
+
+
+
+def main():
+    parser("https://www.theaustralian.com.au/")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(str(e))
